fac3e220aff4136ed28a69712bec1573b117a888
Abhii27 <abhishekpanchal@1998>
2021-09-10 17:11:47 +0530

    add signup and signin

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
+// Importing Env Variables
+require("dotenv").config();
+
 // Libraries
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+
+// microservice routes
+import Auth from "./API/Auth";
+
+
+// Database connection
+import ConnectDB from "./database/connection";
+
+//console.log(process.env);
 
 const zomato = express();
 
@@ -11,6 +23,17 @@ zomato.use(express.urlencoded({ extended: false }));
 zomato.use(helmet());
 zomato.use(cors());
 
+// Application Routes
+zomato.use("/auth", Auth);
+
 zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
 
-zomato.listen(4000, () => console.log("Server is Running!!"));
+const port = process.env.PORT || 4000;
+
+zomato.listen(port, () =>
+  ConnectDB()
+    .then(() => console.log("Server is running 🚀"))
+    .catch(() =>
+      console.log("Server is running, but database connection failed... ")
+    )
+);
